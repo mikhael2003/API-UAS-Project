6497398e78a944a0a6260246941fe73e117ca020
mikhael2003 <mikhaelsiman2003@gmail.com>
2022-12-10 08:44:43 +0700

    update file app and env

---
+++ node.js/app.js
@@ -0,0 +1,22 @@
+// definisikan module, middleware
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+require('dotenv/config')
+
+mongoose.connect(process.env.DB_CONNECRION, { useNewUrlParser: true, useUnifiedTopology: true})
+let db = mongoose.connection
+
+    //handle error
+    db.on('error', console.error.bind(console, 'Error Establishing a Database Connection?'))
+    //handle sucsess 
+    db.once('open', () => {
+        console.log(`Database is connected`)
+    })
+
+
+app.listen(process.env.PORT, () => {
+    console,log(`Server running on ${prcess.env.PORT}`)
+})
+
